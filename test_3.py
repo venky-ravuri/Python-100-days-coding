aa0da152d48ec63fffbe71028a413fa3d0aa8b92
venky <ravuri.venkatesh04@gmail.com>
2021-10-02 14:06:03 +0530

    modified file updation

---
+++ test_3.py
@@ -0,0 +1,7 @@
+def bio():
+    """This function takes your bio data and prints"""
+    name = input("Your name: ")
+    age = int(input("Your age: "))
+    school = input("Name of your college/University: ")
+    section = input("Your section: ")
+
